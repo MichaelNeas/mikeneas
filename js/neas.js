0c4014182f3eb57bf6f2426f2f475e3ae2f59775
Michael Neas <michael.r.neas@gmail.com>
2016-09-22 23:59:03 -0400

    Naive caching implemented

--- js/neas.js
+++ js/neas.js
@@ -1,51 +1,67 @@
 /*neas.js
 * Michael Neas
 */
+(function () {
 
-function setActiveLink(fragmentId){
-    var navLocation = document.getElementById("navbar"),
-        links = navLocation.children,
-        i, link, pageName;
-    for(i = 0; i < links.length; i++){
-        link = links[i];
-        pageName = link.getAttribute("href").substr(1);
-        if(pageName === fragmentId){
-            link.setAttribute("id", "activeButton");
-        } else{
-            link.removeAttribute("id");
+    var loadedPageCache = {};
+
+    function setActiveLink(fragmentId){
+        var navLocation = document.getElementById("navbar"),
+            links = navLocation.children,
+            i, link, pageName;
+        for(i = 0; i < links.length; i++){
+            link = links[i];
+            pageName = link.getAttribute("href").substr(1);
+            if(pageName === fragmentId){
+                link.setAttribute("id", "activeButton");
+            } else{
+                link.removeAttribute("id");
+            }
         }
     }
-}
 
-function getContent(fragmentId, callback){
-    //ajax request
-    var request = new XMLHttpRequest();
-    //callback with content from file
-    request.onload = function(){
-        callback(request.responseText);
+    function fetchFile(path, callback){
+        //ajax request
+        var request = new XMLHttpRequest();
+        //callback with content from file
+        request.onload = function(){
+            callback(request.responseText);
+        }
+        //fetch partial html
+        request.open("GET", path);
+        request.send(null);
     }
-    //fetch partial html
-    request.open("GET", "pages/" + fragmentId + ".html");
-    request.send(null);
-}
 
-function navigation(){
-    var mainContent = document.getElementById("mainContent");
-    //remove hash tag
-    fragmentId = location.hash.substr(1);
-    //passing callback function for async calls
-    getContent(fragmentId, function(content){
-        mainContent.innerHTML = content;
-    });
-    setActiveLink(fragmentId);
-}
+    function getContent(fragmentId, callback){
+        if(loadedPageCache[fragmentId]){
+            callback(partialsCache[fragmentId]);
+        } else{
+            var path = "pages/" + fragmentId + ".html";
+            fetchFile(path, function (content){
+                partialsCache[fragmentId] = content;
+                callback(content);
+            });
+        }
+    }
 
-//if no location hash, send em home
-if(!location.hash){
-    location.hash = "#home";
-}
+    function navigation(){
+        var mainContent = document.getElementById("mainContent");
+        //remove hash tag
+        fragmentId = location.hash.substr(1);
+        //passing callback function for async calls
+        getContent(fragmentId, function(content){
+            mainContent.innerHTML = content;
+        });
+        setActiveLink(fragmentId);
+    }
 
-//initial hash
-navigation();
+    //if no location hash, send em home
+    if(!location.hash){
+        location.hash = "#home";
+    }
 
-window.addEventListener("hashchange", navigation);
+    //initial hash
+    navigation();
+
+    window.addEventListener("hashchange", navigation);
+}());
