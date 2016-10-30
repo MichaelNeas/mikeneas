ea39db3f077b80999a22de5dba0bdd88704b325c
Michael Neas <michael.r.neas@gmail.com>
2016-10-30 14:33:25 -0400

    Add photos and restructuring

--- js/neasAnimate.js
+++ js/neasAnimate.js
@@ -2,39 +2,29 @@
 *  Animations and styling
 */
 
-function displayInformation(){
-	var imgs = document.images,
-    len = imgs.length,
-    counter = 0;
-
-	[].forEach.call( imgs, function( img ) {
-	    img.addEventListener( 'load', incrementCounter, false );
-	    console.log('hi')
-	} );
-
-	function incrementCounter() {
-	    counter++;
-	    if ( counter === len ) {
-	        console.log( 'All images loaded!' );
-	    }
-	}
+function displayInformation(aye){
+	console.log(aye);
+    aye.nextElementSibling.style.display = "block";
+    aye.onmouseleave = (function(){
+        aye.nextElementSibling.style.display = "none";
+    })
 }
+
 
 function setBackground(fragmentId){
     //console.log(fragmentId);
     switch(fragmentId){
-        case 'fitness':
+        case 'life':
             document.body.id = 'fitnessBackground';
             break;
         case 'about':
             document.body.id= 'aboutBackground';
             break;
-        case 'adventure':
+        case 'showcase':
             document.body.id = 'adventureBackground';
             break;
         case 'development':
             document.body.id = 'developmentBackground';
-            displayInformation();
             break;
         default:
             document.body.id = 'landingBackground';
