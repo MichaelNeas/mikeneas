d486d0738f576026a94ca1cf353b646c801ac7c0
Michael Neas <michael.r.neas@gmail.com>
2017-12-03 19:08:06 -0500

    Came up with a cool idea to build out the website, throwing the about information in home, enhancing previous about navigation to display cv

--- dist/js/neasAnimate.js
+++ dist/js/neasAnimate.js
@@ -38,7 +38,35 @@ function setBackground(fragmentId) {
     window.addEventListener('scroll', function () {
         if (window.location.hash === '#home') {
             const windowScroll = window.pageYOffset !== undefined ? window.pageYOffset : (document.documentElement || document.body.parentNode || document.body).scrollTop;
-            document.querySelector('.header-logo').style.transform = 'translate(0px, ' + windowScroll / 8 + '%)';
+            document.querySelector('.header-logo').style.transform = 'translate(0px, ' + windowScroll / 4 + '%)';
         }
     });
+
+    const startDateTime = new Date(1993, 3, 21, 4, 42, 52, 0); // YYYY (M-1) D H m s ms (start time and date from DB)
+    const startStamp = startDateTime.getTime();
+
+    let newDate = new Date();
+    let newStamp = newDate.getTime();
+    let timer;
+
+    function updateClock() {
+        newDate = new Date();
+        newStamp = newDate.getTime();
+        let diff = Math.round((newStamp - startStamp) / 1000);
+
+        let d = Math.floor(diff / (24 * 60 * 60));
+        diff = diff - d * 24 * 60 * 60;
+        let h = Math.floor(diff / (60 * 60));
+        diff = diff - h * 60 * 60;
+        let m = Math.floor(diff / 60);
+        diff = diff - m * 60;
+        let s = diff;
+        if (window.location.hash === '#home') {
+            document.getElementById("time-elapsed").innerHTML = d + " day(s), " + h + " hour(s), " + m + " minute(s), " + s + " second(s) spent alive";
+        }
+    }
+
+    setInterval(updateClock, 1000);
+
+    const contactButton = document.querySelector('.bouncy-button');
 })();
